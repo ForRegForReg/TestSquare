f029130218f97848a5eb0982ae1cbf83feb489a2
регистрации для <for_reg_for_reg@mail.ru>
2018-10-16 18:48:33 +0300

    Add project files.

---
+++ TestSquare/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestSquare
+{
+    public class Class1
+    {
+    }
+}
